122892e61d9ef7057d1d87cadc2b500ffbc1c4cb
Brad Guesman <bradguesman@brads-mbp.devices.brown.edu>
2020-08-19 15:15:44 -0700

    Plopped some files from the wetnet project as a starting point, since the train loop is pretty similar.

---
+++ preprocess.py
@@ -0,0 +1,11 @@
+import numpy as np
+import tensorflow as tf
+
+def get_data(file_directory):
+	"""
+	This is where we should load the data from the file and preprocess it.
+	
+	Typically in DL we'd also split it into training and testing examples.
+	So we'd return something like 4 numpy arrays:
+		return train_inputs, train_outputs, test_inputs, test_output
+	"""
